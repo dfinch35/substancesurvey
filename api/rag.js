6b5de8def3f999b56dab3b8561fd9f9fe23fa753
dfinch35 <davefinch@northstaraudioinnovations.com>
2026-03-18 19:48:38 -0500

    Update CORS headers to allow all origins and methods

--- api/rag.js
+++ api/rag.js
@@ -7,6 +7,8 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://brass-piano-cckj.squarespace.com");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
